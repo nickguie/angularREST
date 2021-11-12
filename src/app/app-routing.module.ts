d7dc6df52efa697e3ae9b2d389e732ce756eab3e
Henrique Carvalho de Souza <niguiealmeida@hotmail.com>
2021-11-12 16:14:57 -0300

    angular REST

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ClienteComponent } from './view/cliente/cliente.component';
+import { EnderecoComponent } from './view/endereco/endereco.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'endereco',
+    component:EnderecoComponent
+  },
+  {
+    path:'cliente',
+    component: ClienteComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
